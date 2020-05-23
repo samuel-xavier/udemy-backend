b0ebf3197f17b339367a0e5291ab158f460aa49a
samuel-xavier <samueloliveira.xavier@hotmail.com>
2020-05-23 16:54:01 -0300

    v3

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/todo';
-mongoose.connect(url, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 
 module.exports = mongoose;
